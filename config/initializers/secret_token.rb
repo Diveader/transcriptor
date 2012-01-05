1b35ee05e9f3adbf20f052925f64c3b6e74d0df7
Diveader <o40@mail.ru>
2012-01-05 05:41:02 +0200

    version 0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transcript::Application.config.secret_token = 'cfb226e44601c31e74fa79d3d24cf6c75d07346edac650cb3bf7aab4b6c3a08e42b662805277c27a3db803d51b921c2669f5f163c90167cb63130c661412f67b'
